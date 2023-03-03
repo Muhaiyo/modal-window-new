2302c0148a00dc29743a92ce46239c04507b97e3
Muhaiyo <kiritokirito184@gmail.com>
2023-03-03 17:57:31 +0100

    Animation transition group
    Add animation from delet from list, addtion and sorting

--- src/components/postList.vue
+++ src/components/postList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
+    <transition-group name="post-list">
     <post-item
         v-for="post in posts"
         :post="post"
         :key="post.id"
         @remove="$emit('remove', post)"
     />
+    </transition-group>
   </div>
   <h2 v-else style="color: red">
     Ты удалил все посты !!
@@ -27,5 +29,20 @@ export default {
 </script>
 
 <style scoped>
-
+.post-list-item{
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active{
+  transition: all 0.4s ease;
+}
+.post-list-enter-form,
+.post-list-leave-to{
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
 </style>
